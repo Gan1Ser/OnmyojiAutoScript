dade58af7c55f57efabdf7dd4a18cf2a6a58137a
GanSer <1742158140@qq.com>
2025-07-16 17:18:17 +0800

    fix(Activity):update

--- tasks/ActivityShikigami/script_task.py
+++ tasks/ActivityShikigami/script_task.py
@@ -150,7 +150,8 @@ class ScriptTask(GameUi, BaseActivity, SwitchSoul, ActivityShikigamiAssets):
                 break
             if self.appear_then_click(self.I_SHI, interval=1):
                 continue
-
+            if self.ocr_appear_click(self.O_ENTRY_ACTIVITY, interval=1):
+                continue
             if self.appear_then_click(self.I_BATTLE, interval=1):
                 continue
             if self.appear_then_click(self.I_BATTLE_1, interval=1):
@@ -171,6 +172,8 @@ class ScriptTask(GameUi, BaseActivity, SwitchSoul, ActivityShikigamiAssets):
             if self.appear_then_click(self.I_UI_BACK_YELLOW, interval=2):
                 continue
             if self.appear_then_click(self.I_BACK_GREEN, interval=2):
+                continue
+            if self.appear_then_click(self.I_EXIT, interval=2.2, threshold=0.6):
                 continue
 
     def check_ap_remain(self, current_ap: ApMode) -> bool:
