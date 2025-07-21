030b259a8931fb1d00ea99901132023e7248cf8f
Rain1231244 <raosiz@deakin.edu.au>
2025-07-21 17:30:14 +0800

    add background only and fix bug

--- tasks/Script/config_device.py
+++ tasks/Script/config_device.py
@@ -70,6 +70,12 @@ class Device(BaseModel):
     emulatorinfo_path: str = Field(default='',
                                    description='emulatorinfo_path_help')
     # 举例, E:\ProgramFiles\MuMuPlayer-12.0\shell\MuMuPlayer.exe
+    # 模拟器启动后最小化
+    emulator_window_minimize: bool = Field(default=False,
+                                             description='emulator_window_minimize_help')
+    # 启动时纯后台运行模拟器，不显示窗口和任务栏
+    run_background_only: bool = Field(default=False,
+                                             description='run_background_only_help')
 
 
 
