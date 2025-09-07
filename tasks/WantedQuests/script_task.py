0932305e49d97d17f15dd9558d098cd736421046
Gan1Ser <1742158140@qq.com>
2025-09-07 17:33:37 +0800

    fix(WantedQuests): Fix battle end dialogue bug

--- tasks/WantedQuests/script_task.py
+++ tasks/WantedQuests/script_task.py
@@ -432,6 +432,7 @@ class ScriptTask(WQExplore, SecretScriptTask, WantedQuestsAssets):
                         self.device.click_record_clear()
                     continue
             success = self.run_general_battle(self.battle_config)
+            self.ui_click(self.C_SECRET_CHAT, self.I_WQSE_FIRE)
         while 1:
             self.screenshot()
             if self.appear(self.I_CHECK_EXPLORATION):
